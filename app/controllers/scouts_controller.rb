77efc24885885fda8d75f49a940f35f1583ab281
Kevin Primm <kfprimm@sys87.co>
2017-03-18 06:13:06 -0400

    scout form improvements

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -1,14 +1,16 @@
 class ScoutsController < AuthenticatedController
+  before_action :set_scout, except: [:index, :new, :create]
+
   def index
     enforce_view_permission(Scout)
     @scouts = Scout.order('lower(first_name), lower(last_name)')
-    
+
     if params[:active] == 'true'
       @scouts = @scouts.active
     elsif params[:active] == 'false'
       @scouts = @scouts.inactive
     end
-    
+
     if params[:admin] == 'true'
       @scouts = @scouts.admin
     elsif params[:admin] == 'false'
@@ -21,22 +23,21 @@ class ScoutsController < AuthenticatedController
   end
 
   def create
-    @scout = Scout.create scout_params
-    redirect_to scouts_url
-  end
-
-  def edit
-    @scout = Scout.find_by id: params[:id]
+    @scout = Scout.new scout_params
+    if @scout.save
+      redirect_to scouts_url, flash: { success: 'Account created!' }
+    else
+      render 'new'
+    end
   end
 
   def update
-    @scout = Scout.find_by id: params[:id]
     @scout.update_attributes scout_params
-    
+
     respond_to do |format|
       format.html do
         if @scout.valid?
-          redirect_to scouts_url
+          redirect_to scouts_url, flash: { success: 'Account created!' }
         else
           render 'edit'
         end
@@ -44,11 +45,15 @@ class ScoutsController < AuthenticatedController
       format.js
     end
   end
-  
-  
+
   private
 
   def scout_params
     params.require(:scout).permit(:first_name, :last_name, :nickname, :email, :password, :is_active, :slot, :is_admin)
+  end
+
+  def set_scout
+    @scout = Scout.find_by id: params[:id]
+    redirect_to scouts_path, flash: { danger: "Scout not found." } if @scout.nil?
   end
 end
