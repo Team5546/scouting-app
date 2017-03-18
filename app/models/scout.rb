6437f2b34b0ff23513f4edd677609602a8822a14
Kevin Primm <kfprimm@sys87.co>
2017-03-18 01:03:07 -0400

    ugh

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -4,34 +4,34 @@ class Scout < ApplicationRecord
   has_many :reports
   scope :active, -> { where(is_active: true) }
   scope :inactive, -> { where(is_active: false) }
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  
+
   has_secure_password
 
   def name
     [first_name, nickname.present? ? "\"#{nickname}\"" : nil, last_name].join(' ')
   end
-  
-  enum slot: [:r1, :r2, :r3, :b1, :b2, :b3 ]
-  
+
+  enum slot: [:r1, :r2, :r3, :b1, :b2, :b3]
+
   def self.is_assigned?(slot)
     find_by(slot: slot)
   end
-  
+
   def self.is_admin?(slot)
     find_by(slot: slot)
   end
-  
+
   def self.viewable_by?(user)
     user.is_admin?
   end
-  
+
   def find_team_for_match(match)
     return nil if slot.nil?
-    
+
     color, number = slot.match(/([rb])([1-4])/).to_a.slice(1, 2)
     number = number.to_i - 1
     color = { "r" => "red", "b" => "blue" }[color]
--- db/migrate/20170318044855_fix_slot_type.rb
+++
@@ -1,5 +0,0 @@
-class FixSlotType < ActiveRecord::Migration[5.0]
-  def change
-    change_column :scouts, :slot, :string
-  end
-end
